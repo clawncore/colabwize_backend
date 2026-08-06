069e6ef95c1b1b57d9197662ef137d610c9a9661
clawncore <clawncoremultitech@gmail.com>
2026-08-06 23:20:51 +0530

    fix: add dist to gitignore, clean TS errors

--- dist/publishing/templates/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.templateToExportSettings = templateToExportSettings;
-/** Map a resolved template onto the export job settings it implies. */
-function templateToExportSettings(tpl) {
-    return { cslStyle: tpl.cslStyle, enableCiteproc: true };
-}
