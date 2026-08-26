b46345be09046c1feb61f70278c28e62d6506e91
clawncore <clawncoremultitech@gmail.com>
2026-08-26 19:08:12 +0530

    Simby: Backend integration update

--- src/admin/services/cacheService.ts
+++
@@ -1,7 +0,0 @@
-import logger from '../../monitoring/logger';
-
-export async function flushAllCache(): Promise<boolean> {
-  logger.info('Flushing all administrative and system cache...');
-  return true;
-}
-
