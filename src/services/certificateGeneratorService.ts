7e5fd8a225faa96ff8dcec5ff1f656d7250bee4d
clawncore <clawncoremultitech@gmail.com>
2026-08-26 17:16:55 +0530

    Simby: Backend integration update

--- src/services/certificateGeneratorService.ts
+++
@@ -1,4 +0,0 @@
-// This file is deprecated in favor of authorshipCertificateGenerator.ts
-// which now uses HTML-to-PDF with Puppeteer for better design control
-
-export { AuthorshipCertificateGenerator as CertificateGeneratorService } from './authorshipCertificateGenerator';
